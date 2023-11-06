fd383735b6d79a93206922db5fe5fcffcb408a05
Pero <g00422221@atu.ie>
2023-11-06 17:08:11 +0000

    replaces for loops with enhanced for loops

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,4 +1,6 @@
 package ie.atu;
+import java.util.ArrayList;
+
 
 public class ArrayDemo {
     public static void main(String[] args)
@@ -11,21 +13,23 @@ public class ArrayDemo {
     {
         //declares an array of ints
         int[] anArray;
-        int i;
+        int i=0;
 
         //allocates memory for 10 ints
         anArray = new int[10];
 
         //initializes all elements in the array
-        for(i=0;i<10;i++)
+        for(int val: anArray)
         {
             anArray[i] = 100+(i*100);
+            i++;
         }
-
+        i=0;
         //prints out all elements in the array
-        for(i=0;i<10;i++)
+        for(int val: anArray)
         {
             System.out.println("Element at "+ i + " " +"is "+anArray[i]);
+            i++;
         }
     }
 }

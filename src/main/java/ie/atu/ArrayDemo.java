9e3385262bde65c878a7febb97fd7227f8048573
Pero <g00422221@atu.ie>
2023-11-06 16:37:57 +0000

    wrote methode to initalize an array and print out its values

---
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -0,0 +1,26 @@
+package ie.atu;
+
+public class ArrayDemo {
+    public static void main(String[] args)
+    {
+        //declares an array of ints
+        int[] anArray;
+        int i;
+
+        //allocates memory for 10 ints
+        anArray = new int[10];
+
+        //initializes all elements in the array
+        for(i=0;i<10;i++)
+        {
+            anArray[i] = 100+(i*100);
+        }
+
+        //prints out all elements in the array
+        for(i=0;i<10;i++)
+        {
+            System.out.println("Element at "+ i + " " +"is "+anArray[i]);
+        }
+
+    }
+}
